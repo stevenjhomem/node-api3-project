3b372baff771cc60224e699c291045831c64336d
stevenjhomem <sjhomem@outlook.com>
2021-09-29 10:48:08 -0500

    firts couple of tests passed

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const {
-  logger,
   validateUser,
   validateUserId,
   validatePost,
-}=require('../middleware/middleware')
+}=require('../middleware/middleware');
+const User = require('./users-model');
+const Post = require('../posts/posts-model');
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+  .then(users=>{
+    res.json(users)
+  })
+  .catch(next)
 });
 
 router.get('/:id',validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
-  res.send(`${req.user.name}`);
 });
 
 router.post('/',validateUser, (req, res) => {
@@ -30,27 +35,31 @@ router.put('/:id',validateUserId,validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  res.send(`${req.user.name}`);
+  
 });
 
 router.delete('/:id',validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
-  res.send(`${req.user.name}`);
 });
 
 router.get('/:id/posts',validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
-  res.send(`${req.user.name}`);
 });
 
 router.post('/:id/posts',validateUserId,validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  res.send(`${req.user.name}`);
 });
 
+router.use((err, req, res, next)=>{
+  res.status(err.status || 500).json({
+    customMessage: "Something tragic insides posts rotuer happened",
+    message: err.message,
+    stack: err.stack
+  })
+})
 // do not forget to export the router
 module.exports=router;
